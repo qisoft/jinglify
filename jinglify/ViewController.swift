bd178a0b8fea9ce71df10f49712928a2c2d354f3
Innokentiy <qisoft3@gmail.com>
2017-02-01 17:21:30 +0500

    Initial Commit

---
+++ jinglify/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  jinglify
+//
+//  Created by Innokentiy Shushpanov on 01/02/2017.
+//  Copyright © 2017 Innokentiy Shushpanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
