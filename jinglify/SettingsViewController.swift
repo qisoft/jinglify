94a66eed0dca925aab7531cf1318190aa47de4bd
Ilya Sedov <ilyasedov@vl2003.ar14-24.ekb.ru.mirasystem.net>
2017-02-08 10:35:17 +0500

    save settings between launches

--- jinglify/SettingsViewController.swift
+++ jinglify/SettingsViewController.swift
@@ -10,8 +10,46 @@ import UIKit
 import MediaPlayer
 
 struct GameSettings {
-    var matchTime: Double = 5.0
-    var songs = MPMediaItemCollection(items: Array<MPMediaItem>())
+
+    private let matchTimeKey = "matchTime"
+    private let songsKey = "songs"
+
+    init() {
+        let savedMatchTime = UserDefaults.standard.double(forKey: matchTimeKey)
+        if (savedMatchTime == 0.0) {
+            matchTime = 5.0
+        } else {
+            matchTime = savedMatchTime
+        }
+
+        songs = MPMediaItemCollection(items: Array<MPMediaItem>())
+        if let ids = UserDefaults.standard.array(forKey: songsKey) as? [UInt64] {
+            let predicates = ids.map({ (id) -> MPMediaPredicate in
+                return MPMediaPropertyPredicate.init(value: id, forProperty: MPMediaItemPropertyPersistentID)
+            })
+            let query = MPMediaQuery(filterPredicates: Set(predicates))
+            if let qItems = query.items {
+                songs = MPMediaItemCollection(items: qItems)
+            }
+        }
+    }
+
+    var matchTime: Double {
+        didSet {
+            save()
+        }
+    }
+    var songs: MPMediaItemCollection {
+        didSet {
+            save()
+        }
+    }
+
+    private func save() {
+        let ids = songs.items.map { $0.persistentID }
+        UserDefaults.standard.set(ids, forKey: songsKey)
+        UserDefaults.standard.set(matchTime, forKey: matchTimeKey)
+    }
 }
 
 class SettingsViewController: UIViewController, MPMediaPickerControllerDelegate {
@@ -21,13 +59,31 @@ class SettingsViewController: UIViewController, MPMediaPickerControllerDelegate 
     @IBOutlet weak var songTitle: UILabel!
     @IBOutlet weak var songArtist: UILabel!
     @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var timeStepper: UIStepper!
 
     var gameSettings = GameSettings()
 
+    //MARK: - Lifecycle 
 
     override func viewDidLoad() {
         super.viewDidLoad()
         startButton.isEnabled = false
+        showSelectSong()
+        timeStepper.value = gameSettings.matchTime
+        matchTimeChanged(timeStepper)
+    }
+
+    func showSelectSong() {
+        if (gameSettings.songs.count > 0) {
+            let song = gameSettings.songs.items.first
+            songArtist.text = song?.artist ?? "-"
+            songTitle.text = song?.title ?? "-"
+            startButton.isEnabled = true
+            startButton.backgroundColor = UIColor.init(red: 52 / 255,
+                                                       green: 94 / 255,
+                                                       blue: 242 / 255,
+                                                       alpha: 1.0)
+        }
     }
 
     // MARK: - MPMediaPickerControllerDelegate impl
@@ -35,19 +91,8 @@ class SettingsViewController: UIViewController, MPMediaPickerControllerDelegate 
                      didPickMediaItems mediaItemCollection: MPMediaItemCollection)
     {
         mediaPicker.dismiss(animated: true)
-
         gameSettings.songs = mediaItemCollection
-        if(mediaItemCollection.count > 0)
-        {
-            let song = mediaItemCollection.items[0]
-            songArtist.text = song.artist ?? "-"
-            songTitle.text = song.title ?? "-"
-            startButton.isEnabled = true
-            startButton.backgroundColor = UIColor.init(red: 52 / 255,
-                                                       green: 94 / 255,
-                                                       blue: 242 / 255,
-                                                       alpha: 1.0)
-        }
+        showSelectSong()
     }
 
     func mediaPickerDidCancel(_ mediaPicker: MPMediaPickerController) {
